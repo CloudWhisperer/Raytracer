5e99bc73537c81da03703246e58705c6246ae3f3
Shuaib Ahmed <shuaibahmed122@gmail.com>
2022-11-28 23:04:28 +0000

    need help
    stuck

--- MCG_GFX_Framework/Camera.cpp
+++ MCG_GFX_Framework/Camera.cpp
@@ -1,14 +1,18 @@
 #include "Camera.h"
 #include <map>
-using namespace std;
 #include <iostream>
 
-ray camera::camray(glm::vec2 _pixelpos)
+ray camera::camray(glm::vec2 _pixelpos,glm::vec2 _screenres)
 {
-    std::map<float, float> _pixelpos.x;
 
-    const float xnormal = map(static_cast<float>(pixelpos.x),
-        0.0f, static_cast<float>(mscreenresolution.y), 1.0f, -1.0f);
+    //const float xnormal = static_cast<float>(_pixelpos.x), 0.0f, static_cast<float>(_screenres.y), 1.0f, -1.0f;
+
+    //std::map<float, float*>_pixelpos,_screenres;
+    //_pixelpos.x = 0.0f;
+    //_screenres.y = 0.0f;
+
+    //const float xnormal = map(static_cast<float>(pixelpos.x),
+    //0.0f, static_cast<float>(mscreenresolution.y), 1.0f, -1.0f);
 
     return ray();
 }
