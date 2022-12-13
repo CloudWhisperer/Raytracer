7269c85c6c3f0dd6736444288d3821285a6251be
CloudWhisperer <shuaibahmed122@gmail.com>
2022-12-13 18:37:38 +0000

    trying to add multithread,very hard
    sad face

--- MCG_GFX_Framework/Main.cpp
+++ MCG_GFX_Framework/Main.cpp
@@ -8,9 +8,59 @@
 #include "Ray.h"
 #include "Raytracer.h"
 #include "sphere.h"
+#include <thread>
+#include <mutex>
+#include <Windows.h>
+
+std::mutex m;
+camera cam;
+Material spheremat;
+sphere inviscircle = sphere(0.001, glm::vec3(0, 0, 0), spheremat);
+Raytracer tracer;
+ray r;
+
+int loop()
+{
+	m.lock();
+
+	//main loop for the raytracer, checks every pixel
+	for (int y = 0; y < MCG::getwinsize().y; y++)
+	{
+		r = cam.camray(glm::vec2(y));
+		MCG::DrawPixel(glm::vec2(y), tracer.returncol(r));
+
+		std::cout << "1" << std::endl;
+	}
+	m.unlock();
+
+}
+
+int loop2()
+{
+	m.lock();
+	for (int x = 0; x < MCG::getwinsize().x; x++)
+	{
+		r = cam.camray(glm::vec2(x));
+		MCG::DrawPixel(glm::vec2(x), tracer.returncol(r));
+
+		std::cout << "2" << std::endl;
+	}
+	m.unlock();
+}
+
+//void loopfinal()
+//{
+//	m.lock();
+//	ray r = cam.camray(glm::vec2(y,x));
+//	MCG::DrawPixel(glm::vec2(x), tracer.returncol(r));
+//	std::cout << "3" << std::endl;
+//	m.unlock();
+//}
+
 
 int main(int argc, char* argv[])
 {
+
 	// Variable for storing window dimensions
 	glm::ivec2 windowSize(640, 480);
 
@@ -40,28 +90,28 @@ int main(int argc, char* argv[])
 	// Do any other DrawPixel calls here
 	// ...
 
-	camera cam;
-	Material spheremat;
-	sphere inviscircle = sphere(0.001, glm::vec3(0, 0, 0), spheremat);
-	Raytracer tracer;
 
-	//main loop for the raytracer, checks every pixel
-	for (int y = 0; y < MCG::getwinsize().y; y++)
-	{
-		for (int x = 0; x < MCG::getwinsize().x; x++)
-		{
-			ray r = cam.camray(glm::vec2(x, y));
-			MCG::DrawPixel(glm::vec2(x, y), tracer.returncol(r));
-		}
-	}
+	//std::thread t1(loop); std::thread t2(loop2);
+	//t1.join(); t2.join();
 
-	std::cout << " Finished tracing" << std::endl;
+	std::thread myThreads[4];
+
+	myThreads[0] = std::thread(loop);
+	myThreads[1] = std::thread(loop2);
+	myThreads[0].join();
+	myThreads[1].join();
+
+
+
+
+
 
 
 		// Displays drawing to screen and holds until user closes window
-		// You must call this after all your drawing calls
-		// Program will exit after this line
-		return MCG::ShowAndHold();
+	// You must call this after all your drawing calls
+	// Program will exit after this line
+	std::cout << " Finished tracing" << std::endl;
+	return MCG::ShowAndHold();
 
 
 		// Advanced access - comment out the above DrawPixel and MCG::ShowAndHold lines, then uncomment the following:
